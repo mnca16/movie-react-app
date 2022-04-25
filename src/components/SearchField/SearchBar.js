65bf468f4ee7670e099e5f30d62d238d60d0208d
LongJourney47 <95501902+LongJourney47@users.noreply.github.com>
2022-04-25 08:53:30 -0400

    initial commit

--- src/components/SearchField/SearchBar.js
+++ src/components/SearchField/SearchBar.js
@@ -1,50 +1,72 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import MovieData from "./MovieData";
+import axios from "axios";
 // search field
 
-const movieData = [
-    {
-      id:'i1',
-      title: 'Batman Dark Knight',
-      awards: '6',
-      releaseDate: '2016'
-    },{
-      id:'i2',
-      title: 'Brother Bears',
-      awards: '9',
-      releaseDate: '2000'
-    },{
-      id:'i3',
-      title: 'Star Wars Phantom Menace',
-      awards: '5',
-      releaseDate: '2003'
-    },{
-      id:'i4',
-      title: 'Purge Forever',
-      awards: '2',
-      releaseDate: '2022'
-    },
-  ]
-
-
 const SearchBar = () => {
-    const handlerOnChange = (event) => {
-        console.log(event.target.value)
-    }
-    
-    const [title, setTitle] = useState('')
+  const [searchInput, setSearchInput] = useState("");
+  const [movieData, setMovieData] = useState([]);
 
-    cosnt [userResults, setUserResults] = useState('')
+  useEffect(() => {
+    // axios
+    //   .get("http://www.omdbapi.com/", {
+    //     params: {
+    //       // personal key needed to use api, 1000 data collection attempts a day
+    //       apikey: "15385f9a",
+    //     },
+    //   })
+    //   .then((response) => {
+    //     setMovieData(response.data);
+    //   });
+    axios.get("https://www.swapi.tech/api/people/").then((response) => {
+      setMovieData(response.data);
+      console.log(response); //causes infinite data collection
+    }); // the [] prevents the issue above
+  }, []);
 
-    return (
-        <div>
+  const searchMovies = (searchValue) => {
+    // const movieData = searchValue.target.value;
+    // console.log(searchValue.target.value);
+    setSearchInput(searchValue);
+    const filteredData = movieData.filter((movie) => {
+      return Object.values(movie)
+        .join("")
+        .toLocaleLowerCase()
+        .includes(searchInput.toLocaleLowerCase());
+    });
+    console.log(searchValue); //testing functionality of search
+    console.log(filteredData);
+  };
+
+  return (
+    <div>
+      <div className="search-field">
         <label htmlFor="search">Search a movie you like!</label>
-        <input id="search" onChange={handlerOnChange}/>
-        </div>
-    )
-}
+        <input
+          type="search"
+          onChange={(event) => searchMovies(event.target.value)}
+          // onChange={searchMovies}
+          className="input-field"
+        />
+        <span>
+          <button type="button">Enter</button>
+        </span>
+      </div>
 
-
-
-
+      {/* <div className="movie-list">
+        {userResults && userResults.legnth > 0 ? (
+          userResults.map((movie) => (
+            <li key={movie.id} className="movie">
+              <span>{movie.id}</span>
+              <span>{movie.title}</span>
+            </li>
+          ))
+        ) : (
+          <h1>No Results Discovered</h1>
+        )}
+      </div> */}
+    </div>
+  );
+};
 
 export default SearchBar;
